468a4acf701f41c1015ac07920fc5ea790ae5e00
aigoncharov <andrey@goncharov.page>
2025-04-08 16:36:11 +0300

    Remove quantization for Mistral 24b

--- src/experiments/single_token_entropy_mmlu_mistral_24b.py
+++ src/experiments/single_token_entropy_mmlu_mistral_24b.py
@@ -1,8 +1,7 @@
 import ast
 from pathlib import Path
 
-import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, TorchAoConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from reasoning_fine_tune.entropy_estimation.estimate_dataset import estimate_dataset
 from reasoning_fine_tune.utils.device import DEVICE
@@ -13,12 +12,8 @@ MODEL_NAME = "mistralai/Mistral-Small-24B-Instruct-2501"
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-quantization_config = TorchAoConfig("int4_weight_only", group_size=128)
 model = AutoModelForCausalLM.from_pretrained(
     MODEL_NAME,
-    low_cpu_mem_usage=True,
-    torch_dtype=torch.bfloat16,
-    quantization_config=quantization_config,
     device_map=DEVICE,
 )
 
