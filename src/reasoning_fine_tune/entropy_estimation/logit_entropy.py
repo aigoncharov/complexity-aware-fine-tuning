Fix single token entropy calculation

src/reasoning_fine_tune/entropy_estimation/logit_entropy.py
@@ -1,47 +1,21 @@
-from dataclasses import dataclass
-from typing import Any
-
 import torch
 
-LLMModel = Any
 
+def compute_entropy_from_logits(logits: torch.Tensor) -> float:
+    """
+    Compute entropy from logits.
 
-# TODO: Cite https://github.com/abazarova/tda4hallucinations/
-@dataclass
-class TokenwiseEntropy:
-    llm_model: LLMModel
+    Parameters:
+    ----------
+    logits : torch.Tensor
+        Logits from the model.
 
-    @torch.no_grad()
-    def calculate(self, input_ids) -> float:
-        token_distribution = self._get_token_distribution(input_ids)
-        entropy = self._compute_entropy_from_logits(token_distribution)
-        return entropy.detach().cpu().item()
-
-    def _get_token_distribution(self, input_ids) -> torch.Tensor:
-        # Yield the output of the model for the current example
-        output = self.llm_model(
-            input_ids,
-            output_hidden_states=True,
-            output_attentions=False,
-        )
-
-        return output.logits[0, -1:]
-
-    def _compute_entropy_from_logits(self, logits: torch.Tensor) -> torch.Tensor:
-        """
-        Compute entropy from logits.
-
-        Parameters:
-        ----------
-        logits : torch.Tensor
-            Logits from the model.
-
-        Returns:
-        -------
-        torch.Tensor
-            Entropy values.
-        """
-        probabilities = torch.softmax(logits, dim=-1)
-        log_probabilities = torch.log(probabilities + 1e-12)
-        entropies = -torch.sum(probabilities * log_probabilities, dim=-1)
-        return entropies[0]
+    Returns:
+    -------
+    torch.Tensor
+        Entropy values.
+    """
+    probabilities = torch.softmax(logits, dim=-1)
+    log_probabilities = torch.log(probabilities + 1e-12)
+    entropy = -torch.sum(probabilities * log_probabilities, dim=-1)
+    return entropy.item()