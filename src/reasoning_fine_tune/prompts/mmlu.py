78d359648d55ea07245bcfc6b0514e292327852c
aigoncharov <andrey@goncharov.page>
2025-04-21 13:26:02 +0300

    Add mistral analysis

--- src/reasoning_fine_tune/prompts/mmlu.py
+++ src/reasoning_fine_tune/prompts/mmlu.py
@@ -21,6 +21,16 @@ def single_token_sys_prompt_with_fallback_for_unknown_answers(subject: str | Non
     return sys_msg
 
 
+def single_token_sys_prompt_with_fallback_for_unknown_answers_alternative(subject: str | None = None):
+    if subject is not None:
+        sys_msg = f"The following are multiple choice questions about {subject}."
+    else:
+        sys_msg = "The following are multiple choice questions."
+
+    sys_msg += " If you know the answer return the correct option number, otherwise return 0. Write down ONLY the NUMBER and nothing else."
+    return sys_msg
+
+
 option_ids = [str(i + 1) for i in range(20)]
 
 
@@ -33,4 +43,10 @@ def single_token_answer_prompt(question: str, options: List[str]):
 def single_token_answer_prompt_with_fallback_for_unknown_answers(question: str, options: List[str]):
     options_str = "\n".join([f"{option_id}. {answer}".strip() for option_id, answer in zip(option_ids, options)])
     user_prompt = f"Question: {question.strip()}\nOptions:\n{options_str}\nChoose one of the answers. If you are certain about the answer return the correct option number, otherwise return 0. Write down ONLY the NUMBER and nothing else."
+    return user_prompt
+
+
+def single_token_answer_prompt_with_fallback_for_unknown_answers_alternative(question: str, options: List[str]):
+    options_str = "\n".join([f"{option_id}. {answer}".strip() for option_id, answer in zip(option_ids, options)])
+    user_prompt = f"Question: {question.strip()}\nOptions:\n{options_str}\nChoose one of the answers. If you know the answer return the correct option number, otherwise return 0. Write down ONLY the NUMBER and nothing else."
     return user_prompt
