a5096493a7eea6a3f6d353a859c040459b16c091
aigoncharov <andrey@goncharov.page>
2025-07-16 17:09:05 +0100

    Migrate answer correctness check and mark old data as legacy

--- src/reasoning_fine_tune/utils/correctness.py
+++ src/reasoning_fine_tune/utils/correctness.py
@@ -1,5 +1,8 @@
+from reasoning_fine_tune.prompts.mmlu_option_ids import option_ids
+
+
 def check_answer_correct_mmlu(row, model_answer):
     try:
-        return int(row["answer_index"]) + 1 == int(model_answer.strip())
+        return option_ids[int(row["answer_index"])] == model_answer.strip()
     except:
         return False
